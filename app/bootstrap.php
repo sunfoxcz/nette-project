9719ebe3d06eb6893a5f967001cedba8d704b8b7
Tomas Jacik <foxycode@sunfox.cz>
2016-11-30 02:55:54 +0100

    Bootstrap Added debug mode enabling by NETTE_DEVEL environment variable

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -6,12 +6,13 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->enableDebugger(__DIR__ . '/../log');
+if (getenv('NETTE_DEVEL') === '1') {
+	$configurator->setDebugMode(TRUE);
+}
 
 $configurator->setTimeZone('Europe/Prague');
+$configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
-
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__)
 	->register();
