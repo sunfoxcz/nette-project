20b998e25c897a98a869dd206c7a354b8d2cad44
Tomas Jacik <tomas@jacik.cz>
2020-11-27 07:05:33 +0100

    CS: update indentation to spaces

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -9,23 +9,23 @@ use Nette\Configurator;
 
 class Bootstrap
 {
-	public static function boot(): Configurator
-	{
-		$configurator = new Configurator;
+    public static function boot(): Configurator
+    {
+        $configurator = new Configurator;
 
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy(__DIR__ . '/../var/log');
+        //$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+        $configurator->enableTracy(__DIR__ . '/../var/log');
 
-		$configurator->setTimeZone('Europe/Prague');
-		$configurator->setTempDirectory(__DIR__ . '/../var/temp');
+        $configurator->setTimeZone('Europe/Prague');
+        $configurator->setTempDirectory(__DIR__ . '/../var/temp');
 
-		$configurator->createRobotLoader()
-			->addDirectory(__DIR__)
-			->register();
+        $configurator->createRobotLoader()
+            ->addDirectory(__DIR__)
+            ->register();
 
-		$configurator->addConfig(__DIR__ . '/config/common.neon');
-		$configurator->addConfig(__DIR__ . '/config/local.neon');
+        $configurator->addConfig(__DIR__ . '/config/common.neon');
+        $configurator->addConfig(__DIR__ . '/config/local.neon');
 
-		return $configurator;
-	}
+        return $configurator;
+    }
 }
--- www/index.php
+++ www/index.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 require __DIR__ . '/../vendor/autoload.php';
 
 App\Bootstrap::boot()
-	->createContainer()
-	->getByType(Nette\Application\Application::class)
-	->run();
+    ->createContainer()
+    ->getByType(Nette\Application\Application::class)
+    ->run();
