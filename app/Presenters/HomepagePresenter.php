2078c446016443ac047b9560c2b3fd250056c6bc
Tomas Jacik <tomas@jacik.cz>
2021-01-31 12:35:22 +0100

    Nette: updated to nette 3.1, latte 2.9, orm 0.8, console 0.9

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,8 +5,26 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nettrine\ORM\EntityManagerDecorator;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    /**
+     * @var EntityManagerDecorator
+     */
+    private $em;
+
+    public function __construct(EntityManagerDecorator $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
+
+    public function actionDefault(): void
+    {
+        $this->template->mysqlVersion = $this->em->getConnection()
+            ->executeQuery('SELECT VERSION()')
+            ->fetchOne();
+    }
 }
